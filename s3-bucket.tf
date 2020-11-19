02e77c9dd83917ab068a4a3688b8e606f3df5bb1
therjuh <74712645+therjuh@users.noreply.github.com>
2020-11-19 14:31:24 +0200

    Delete s3-bucket.tf

--- s3-bucket.tf
+++
@@ -1,8 +0,0 @@
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "1.16.0"
-  # insert the 5 required variables here
-  bucket ="terraformed"
-  bucket_prefix = "therjuh"
-
-}
